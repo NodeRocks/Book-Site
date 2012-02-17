8175d18f94cea9a70676a716348da7ea9f8d19cc
Garth Braithwaite <garthdb@gmail.com>
2012-02-17 11:36:16 -0800

    checking node version on heroku

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
  
 app.get('/', function(request, response) {
-  response.send('hello, world');
+	response.send('<p>Version: '+process.version+'</p>')
 });
  
 var port = process.env.PORT || 3000;
